e6b77cbc8b8d2107e454f82c29e6b36631b20f6b
Curtis Cheung <curtis.cheung94@gmail.com>
2022-11-17 15:24:15 -0800

    starting the database and user endpoints for login

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import UserModels, Accounts, JobPostForm
+import services as _services
 
 ## Registration for models similar to Django admin
 app = FastAPI()
@@ -7,3 +8,6 @@ app = FastAPI()
 app.include_router(UserModels.router),
 app.include_router(Accounts.router),
 app.include_router(JobPostForm.router)
+
+
+_services.create.database() ##creates a database
