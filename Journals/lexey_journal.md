7ea539e1189e6842899191ce1d665d31e8f3ced1
Lexey Olsen <lexey.olsen24@gmail.com>
2022-11-22 14:16:56 -0800

    added user info tables

---
+++ Journals/lexey_journal.md
@@ -0,0 +1,22 @@
+## November 21, 2022
+
+Today, I worked on:
+
+* Setting up the tables for an employee user
+
+I was able to set up the sables with a foreign key relation 
+to the accounts model. Which will later hopefully accomplish the 
+ability to be able to let the user edit their profile to represent 
+themselves.
+
+Not sure how to set up the ability to add a profile picture, and how 
+it would be stored in the database, but something I have researched 
+a little bit. Another thing later on I'll add is the connection to 
+user feedback.
+
+
+## November 22, 2022
+
+Today, I worked on:
+
+* Creating endpoints for the user info section
--- api/migrations/002_accounts_table.py
+++ api/migrations/002_accounts_table.py
@@ -15,4 +15,40 @@ steps = [
         DROP TABLE accounts;
         """,
     ],
+    [
+        ## Create the table
+        
+        ## Add profile_picture
+        
+        """
+        CREATE TABLE user_info (
+            career_title VARCHAR(255),
+            location VARCHAR(255),
+            education VARCHAR(255),
+            about TEXT,
+            account_id INTEGER REFERENCES accounts (id)
+        );
+        """,
+        ## Drop the table
+        """
+        DROP TABLE user_info;
+        """,
+    ],
+    [
+        ## Create the table
+        """
+        CREATE TABLE user_work_history (
+            company VARCHAR(255),
+            from_year SMALLINT,
+            to_year SMALLINT,
+            position VARCHAR(255),
+            position_description TEXT,
+            account_id INTEGER REFERENCES accounts (id)
+        );
+        """,
+        ## Drop the table
+        """
+        DROP TABLE user_work_history;
+        """,
+    ],
 ]
