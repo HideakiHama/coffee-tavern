3d5e53bcc528443153942e3555484bd2c75945b4
Christopher <chrshih875@gmail.com>
2022-11-23 15:07:10 -0800

    Accounts almost done

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -1,35 +1,55 @@
 from pydantic import BaseModel
-from typing import Optional, List, Union
+from typing import Optional, List, Union, Literal
 from queries.pool import pool
 
-
+class Error(BaseModel):
+    message: str
 class Account(BaseModel):
     id: int
     email: str
     hashed_password: str
     user_name: str
-    status: str #have to still modify
+    role: str
 
 class AccountIn(BaseModel):
     email: str
     password: str
     user_name: str
-    status: str #have to still modify
+    role: Literal["Employee", "Employer"]
 
 
 class AccountOut(BaseModel):
     id: int
     email: str
     user_name: str
-
+    role: str
 
 class AccountRepo:
+    def get_all(self) -> Union[List[AccountOut], Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, email, user_name, role
+                        FROM accounts
+                        ORDER BY id
+                        """
+                    )
+                    resultList = list(result)
+                return [
+                    self.account_all(record)
+                    for record in resultList
+                ]
+        except Exception as e:
+            return {"message": "Could not get account"}
+
     def get(self, email: str) -> Optional[Account]:
         with pool.connection() as conn:
             with conn.cursor() as db:
                 result = db.execute(
                     """
-                    SELECT id, email, hashed_password, user_name
+                    SELECT id, email, hashed_password, user_name, role
                     FROM accounts
                     WHERE email = %s
                     """,
@@ -43,6 +63,7 @@ class AccountRepo:
                     email=record[1],
                     hashed_password=record[2],
                     user_name=record[3],
+                    role=record[4]
                 )
 
     def create(self, account: AccountIn, hashed_password: str) -> Account:
@@ -51,15 +72,16 @@ class AccountRepo:
                 result = db.execute(
                     """
                     INSERT INTO accounts
-                        (email, hashed_password, user_name)
+                        (email, hashed_password, user_name, role)
                     VALUES
-                        (%s, %s, %s)
+                        (%s, %s, %s, %s)
                     RETURNING id;
                     """,
                     [
                         account.email,
                         hashed_password,
                         account.user_name,
+                        account.role
                     ],
                 )
                 id = result.fetchone()[0]
@@ -67,5 +89,30 @@ class AccountRepo:
                     id=id,
                     email=account.email,
                     hashed_password=hashed_password,
-                    user_name=account.user_name
+                    user_name=account.user_name,
+                    role=account.role
                 )
+
+    def delete(self, account_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM accounts
+                        WHERE id = %s
+                        """,
+                        [account_id]
+                    )
+                    return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def account_all(self, record):
+        return AccountOut(
+            id=record[0],
+            email=record[1],
+            user_name=record[2],
+            role=record[3]
+        )
--- api/routers/JobPostForm.py
+++ api/routers/JobPostForm.py
@@ -25,7 +25,7 @@ def get_all_job_form(repo: JobFormRepository = Depends()):
 
     return repo.get_all()
 
-@router.get('/get_form/{form_id}', tags=["JobForm"],response_model=Union[JobPostFormOut, Error])
+@router.get('/get_form/{form_id}', tags=["JobForm"], response_model=Union[JobPostFormOut, Error])
 def get_one_job_form(form_id: int, response: Response, repo: JobFormRepository = Depends()) -> JobPostFormOut:
     FormDetail = repo.get_one(form_id)
     if FormDetail is None:
