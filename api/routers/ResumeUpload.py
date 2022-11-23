ec3f14f873974b601a90d8ca1cff100f0c6d33f7
Curtis Cheung <curtis.cheung94@gmail.com>
2022-11-23 12:49:16 -0800

    resume changed in resume branch

--- api/routers/ResumeUpload.py
+++ api/routers/ResumeUpload.py
@@ -3,23 +3,32 @@ from typing import List
 import shutil
 router = APIRouter()
 
-@router.post("/resume/")
-async def create_upload_resume(file: UploadFile = File(...)):
-    if not file:
-        return {"message": "No upload file sent"}
-    else:
-        return {"filename": file.filename}
+
+
+@router.post("/upload_resume/")
+async def resume_upload(file: UploadFile = File(...)):
+    with open(f'{file.filename}', "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"file_name": file.filename}
+
+
+# @router.post("/resume/")
+# async def create_upload_resume(file: UploadFile = File(...)):
+#     if not file:
+#         return {"message": "No upload file sent"}
+#     else:
+#         return {"filename": file.filename}
 
 
 
 ## This is for adding multiple files -- maybe use for profile picture
-@router.post("/img/")
-async def upload_image(files: List[UploadFile] = File(...)):
-    for img in files:
-        with open(f'{img.filename}', "wb") as buffer:
-            shutil.copyfilobj(img.file, buffer)
+# @router.post("/img/")
+# async def upload_image(files: List[UploadFile] = File(...)):
+#     for img in files:
+#         with open(f'{img.filename}', "wb") as buffer:
+#             shutil.copyfilobj(img.file, buffer)
 
-    return {"filename": "Good"}
+#     return {"filename": "Good"}
 
 # @router.post("/upload_resumes/") #uploading multiple?
 # async def upload_resumes(files: List[UploadFile]):
