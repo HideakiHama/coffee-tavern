0fda7a17bd363ed4477a9f225f3560afe1ae827a
Curtis Cheung <curtis.cheung94@gmail.com>
2022-11-22 20:53:46 -0800

    trying out new multifile

--- api/routers/ResumeUpload.py
+++ api/routers/ResumeUpload.py
@@ -1,12 +1,27 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, File
+from typing import List
+import shutil
 router = APIRouter()
 
-@router.post("/upload_resume/")
+@router.post("/resume/")
 async def create_upload_resume(file: UploadFile | None = None):
     if not file:
         return {"message": "No upload file sent"}
     else:
         return {"filename": file.filename}
+
+# @router.post("/resume/")
+# async def upload_resume(files: UploadFile = File(...)):
+#     with open(f'{file.filename}')
+
+## This is for adding multiple files -- maybe use for profile picture
+@router.post("/img/")
+async def upload_image(files: List[UploadFile] = File(...)):
+    for img in files:
+        with open(f'{img.filename}', "wb") as buffer:
+            shutil.copyfilobj(img.file, buffer)
+
+    return {"filename": "Good"}
 
 # @router.post("/upload_resumes/") #uploading multiple?
 # async def upload_resumes(files: List[UploadFile]):
