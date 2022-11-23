665da2b627b0023f37794c846d124643cb0b6b09
Christopher <chrshih875@gmail.com>
2022-11-23 15:27:42 -0800

    jm

--- api/queries/JobForm_queries.py
+++ api/queries/JobForm_queries.py
@@ -7,16 +7,12 @@ from queries.pool import pool
 class Error(BaseModel):
     message: str
 
-<<<<<<< api/queries/JobForm_queries.py
-
-=======
 class JobPostForm(BaseModel):
     id: int
     employer: str
     position: str
     location: str
     tag: str
->>>>>>> api/queries/JobForm_queries.py
 class JobPostFormIn(BaseModel):
     employer: str
     position: str
@@ -73,8 +69,6 @@ class JobFormRepository:
         except Exception as e:
             return {"message": "Could not get that JobForm"}
 
-<<<<<<< api/queries/JobForm_queries.py
-=======
     def get_all(self) -> Union[List[JobPostForm], Error]:
         try:
             with pool.connection() as conn:
@@ -95,7 +89,6 @@ class JobFormRepository:
         except Exception as e:
             return {"message": "Could not get any job form today"}
 
->>>>>>> api/queries/JobForm_queries.py
     def create(self, JobForm: JobPostFormIn) -> Union[List[JobPostFormOut], Error]:
         try:
             # connect the database
@@ -182,12 +175,6 @@ class JobFormRepository:
             id=record[0],
             employer=record[1],
             position=record[2],
-<<<<<<< api/queries/JobForm_queries.py
-            from_date=record[3],
-            to_date=record[4],
-            tag=record[5],
-            description=record[6],
-=======
             location=record[3],
             from_date=record[4],
             to_date=record[5],
@@ -202,5 +189,4 @@ class JobFormRepository:
             position=record[2],
             location=record[3],
             tag=record[4]
->>>>>>> api/queries/JobForm_queries.py
         )
