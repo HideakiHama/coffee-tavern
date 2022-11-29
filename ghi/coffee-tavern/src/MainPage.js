8712801e680129c58c25362cd2690b34c6c8bd61
Curtis Cheung <curtis.cheung94@gmail.com>
2022-11-28 18:09:30 -0800

    "working repo"

---
+++ ghi/coffee-tavern/src/MainPage.js
@@ -0,0 +1,19 @@
+function MainPage() {
+  return (
+    <div className="px-4 py-5 my-5 text-center">
+      <h1 style= {{color:"brown"}} className="display-5 fw-bold">Coffee Tavern</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="lead mb-4">
+          
+          Welcome to the one stop to find your dream job!
+        </p>
+        <img src="https://tinyurl.com/3az84dfy" width="400" height="500" alt="coffee-tavern-logo"></img>
+      </div>
+      <h3 style={{color: "Blue"}}> Create a Profile Today</h3>
+      <h4 style={{color:"Blue"}}> Baristas, Bartenders and more </h4>
+      <h4 style={{color:"Blue"}}>  All are welcome</h4>
+    </div>
+  );
+}
+
+export default MainPage;
