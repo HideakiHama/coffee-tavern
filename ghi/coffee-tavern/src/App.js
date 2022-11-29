ffe5156c53fcb162257ec73c0387dc4204fb8349
Curtis Cheung <curtis.cheung94@gmail.com>
2022-11-29 15:00:47 -0800

    added gitlab-ci.yml file to our project for deployment feature later

--- ghi/coffee-tavern/src/App.js
+++ ghi/coffee-tavern/src/App.js
@@ -23,3 +23,25 @@ function App() {
 }
 
 export default App;
+
+// import './App.css';
+// import { 
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from 'react-router-dom'
+// import Notes from './pages/Notes'
+// import Note from './pages/Note'
+// import Layout from './components/Layout'
+// function App() {
+//   return (
+//     <Router>
+//       <Layout>
+//         <Routes>
+//             <Route path="/" element={<Notes/>} exact/>
+//             <Route path="/:id" element={<Note/>}/>
+//         </Routes>
+//       </Layout>
+//     </Router>
+//   );
+// }
