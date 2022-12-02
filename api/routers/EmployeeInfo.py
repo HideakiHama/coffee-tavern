d9ceca3bee31e872f43d51e01f27a7bbd4f3a547
Lexey Olsen <lexey.olsen24@gmail.com>
2022-12-02 13:14:11 -0800

    refactored employee info to connect to account

--- api/routers/EmployeeInfo.py
+++ api/routers/EmployeeInfo.py
@@ -1,26 +1,39 @@
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Response, HTTPException, status
 from typing import Union
 from queries.EmployeeInfo_queries import EmployeeInfoIn, EmployeeInfoRepo, EmployeeInfoOut, Error
+from queries.accounts import AccountRepo
+from RoleChecker import RoleChecker
 
 router = APIRouter()
+
+checker = RoleChecker("Employer")
 
 @router.post("/users/{account_id}/create_employee_info", tags=["User Info"])
 def create_employee_info(
     employee_info: EmployeeInfoIn,
     account_id: int,
     repo: EmployeeInfoRepo = Depends(),
+    repo1: AccountRepo = Depends(),
+    # checked_role: bool = Depends(checker),
 ) -> EmployeeInfoOut:
-    return repo.create(employee_info, account_id)
+    # credentials_exception = HTTPException(
+    #     status_code=status.HTTP_403_FORBIDDEN,
+    #     detail="You are an employer, you should fill out employer info"
+    # )
+    # if checked_role:
+    info = repo.create(employee_info, account_id).dict()
+    info["account_id"] = repo1.get(account_id).dict()
+    return info
+    # raise credentials_exception
 
-@router.get("/users/{account_id}/get_employee_info", tags=["User Info"], response_model=Union[EmployeeInfoOut, Error])
+@router.get("/users/{account_id}/get_employee_info", tags=["User Info"])
 def get_employee_info_by_id(
-    account_id: int, 
-    response: Response, 
-    repo: EmployeeInfoRepo = Depends()
+    account_id: int,
+    repo: EmployeeInfoRepo = Depends(),
+    repo1: AccountRepo = Depends()
     ) -> EmployeeInfoOut:
-    EmployeeInfo = repo.get_one(account_id)
-    if EmployeeInfo is None:
-        response.status_code = 404
+    EmployeeInfo = repo.get_one(account_id).dict()
+    EmployeeInfo["account_id"] = repo1.get(account_id).dict()
     return EmployeeInfo
 
 @router.put("/users/{account_id}/update_employee_info", tags=["User Info"])
@@ -28,5 +41,8 @@ def update_employee_info(
     employee_info: EmployeeInfoIn,
     account_id: int,
     repo: EmployeeInfoRepo = Depends(),
+    repo1: AccountRepo = Depends()
 ) -> EmployeeInfoOut:
-    return repo.update(employee_info, account_id)
+    Updated = repo.update(employee_info, account_id).dict()
+    Updated["account_id"] = repo1.get(account_id).dict()
+    return Updated
