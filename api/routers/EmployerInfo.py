37fdb2a046bd738ffe83d99f8ed41439f2aaaf1a
Lexey Olsen <lexey.olsen24@gmail.com>
2022-12-02 13:27:13 -0800

    refactored employer info to include account

--- api/routers/EmployerInfo.py
+++ api/routers/EmployerInfo.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, Response
 from typing import List, Optional, Union
 from queries.EmployerInfo_queries import EmployerInfoRepo, EmployerInfoIn, EmployerInfoOut, Error
+from queries.accounts import AccountRepo
 
 router = APIRouter()
 
@@ -9,18 +10,20 @@ def create_employer_info(
     employer_info: EmployerInfoIn,
     account_id: int,
     repo: EmployerInfoRepo = Depends(),
+    repo1: AccountRepo = Depends()
 ) -> EmployerInfoOut:
-    return repo.create(employer_info, account_id)
+    info = repo.create(employer_info, account_id).dict()
+    info["account_id"] = repo1.get(account_id).dict()
+    return info
 
-@router.get('/users/{account_id}/get_employer_info', tags=["User Info"], response_model=Union[EmployerInfoOut, Error])
+@router.get('/users/{account_id}/get_employer_info', tags=["User Info"])
 def get_employer_info_by_id(
     account_id: int, 
-    response: Response, 
-    repo: EmployerInfoRepo = Depends()
+    repo: EmployerInfoRepo = Depends(),
+    repo1: AccountRepo = Depends()
     ) -> EmployerInfoOut:
-    EmployerInfo = repo.get_one(account_id)
-    if EmployerInfo is None:
-        response.status_code = 404
+    EmployerInfo = repo.get_one(account_id).dict()
+    EmployerInfo["account_id"] = repo1.get(account_id).dict()
     return EmployerInfo
 
 @router.put("/users/{account_id}/update_employer_info", tags=["User Info"])
@@ -28,5 +31,8 @@ def update_employer_info(
     employer_info: EmployerInfoIn,
     account_id: int,
     repo: EmployerInfoRepo = Depends(),
+    repo1: AccountRepo = Depends()
 ) -> EmployerInfoOut:
-    return repo.update(employer_info, account_id)
+    Updated = repo.update(employer_info, account_id).dict()
+    Updated["account_id"] = repo1.get(account_id).dict()
+    return Updated
