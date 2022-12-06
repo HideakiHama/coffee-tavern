95b7f7901538aefb77bc948a6ff431a9ba84d223
Christopher <chrshih875@gmail.com>
2022-12-06 14:16:05 -0800

    works

--- api/tests/test_jobform.py
+++
@@ -1,9 +0,0 @@
-## run docker ps -->find container name
-## copy name
-## docker exec <container id> python -m pytest
-## arrage -> taking data and manipulating it so it dosent make any extra steps 
-## act -> acting data triggers the function
-## assert -> making sure its getting the response we should be (status code)
-## clean up -> dependancy overrides
-from unittest import TestCase
-    
