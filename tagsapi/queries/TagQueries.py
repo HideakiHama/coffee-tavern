957776d26488fcf1377e8a21ac782b6b1a562097
Curtis Cheung <curtis.cheung94@gmail.com>
2022-12-05 22:13:28 -0800

    Working tagapi, queries and routes not working

--- tagsapi/queries/TagQueries.py
+++ tagsapi/queries/TagQueries.py
@@ -1,42 +1,42 @@
-from pydantic import BaseModel, ValidationError
-from typing import Optional, List, Union
-from queries.pool import pool
+# from pydantic import BaseModel, ValidationError
+# from typing import Optional, List, Union
+# from queries.pool import pool
 
-class Error(BaseModel):
-    message : str
+# class Error(BaseModel):
+#     message : str
 
-class Tags(BaseModel):
-    id: int
-    tag: str
+# class Tags(BaseModel):
+#     id: int
+#     tag: str
 
-class TagIn(BaseModel):
-    id: int
-    tag: str
+# class TagIn(BaseModel):
+#     id: int
+#     tag: str
 
-class TagOut(BaseModel):
-    id: int
-    tag: str
+# class TagOut(BaseModel):
+#     id: int
+#     tag: str
 
 
-class TagRepository:
-    #get
-    def tag_get(self, Tag_id: int) -> Optional[TagOut]:
-        try:
-            with pool.connections() as conn:
-                with conn.cursor() as db:
-                    result = db.execute(
-                        """
-                        SELECT
-                        id,
-                        tags
-                        """,
-                        [
-                            Tag_id,
-                        ]    
-                    )
-                    record = result.fetchone()
-                    if record is None:
-                        return None
-                    return self.record_to_tag_out(record)
-        except Exception as e:
-            return {"message": "No tags available"}
+# class TagRepository:
+#     #get
+#     def tag_get(self, Tag_id: int) -> Optional[TagOut]:
+#         try:
+#             with pool.connections() as conn:
+#                 with conn.cursor() as db:
+#                     result = db.execute(
+#                         """
+#                         SELECT
+#                         id,
+#                         tags
+#                         """,
+#                         [
+#                             Tag_id,
+#                         ]    
+#                     )
+#                     record = result.fetchone()
+#                     if record is None:
+#                         return None
+#                     return self.record_to_tag_out(record)
+#         except Exception as e:
+#             return {"message": "No tags available"}
