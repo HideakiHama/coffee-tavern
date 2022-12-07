8edb1edd7ed89d3364cf739987c447beb747623c
Curtis Cheung <curtis.cheung94@gmail.com>
2022-12-07 11:11:42 -0800

    testing branch

--- tagsapi/routers/TagRoutes.py
+++ tagsapi/routers/TagRoutes.py
@@ -4,42 +4,61 @@ from typing import List, Union
 
 router = APIRouter()
 
-## POST ##
-# creating a new tag #
-@router.post("/create_tag_form", tags=["TagForm"], response_model=TagOut)
-def create_tag(new_form: TagOut, repo: TagRepository = Depends()):
-    return repo.create(new_form)
+
+@router.post("/api/tag")
+async def post_tag(Tags):
+    return 1
+
+@router.get("/api/tag{id}")
+async def get_tag(Tags):
+    return 1
+
+@router.delete("/api/tag{id}")
+async def delete_tag(Tags):
+    return 1
+
+@router.put("/api/tag{id}")
+async def put_tag(id,data):
+    return 1
 
 
-## GET ##
-# getting detail feedback from employer
-@router.get(
-    "/get-tag/{Tag_id}",
-    tags=["TagForm"],
-    response_model=Union[TagOut, Error],
-)
-def get_one_employer_feedback_form(
-    Tag_id: int,
-    response: Response,
-    repo: TagRepository = Depends(),
-) -> TagOut:
-    Tag = repo.get_one(Tag_id)
-    if Tag is None:
-        response.status_code = 404
-    return Tag
+
+# ## POST ##
+# # creating a new tag #
+# @router.post("/create_tag_form", tags=["TagForm"], response_model=TagOut)
+# def create_tag(new_form: TagOut, repo: TagRepository = Depends()):
+#     return repo.create(new_form)
 
 
-## GET ##
-# getting list of tags
-@router.get(
-    "/get_all_tags", tags=["TagForm"], response_model=Union[List[TagOut], Error]
-)
-def get_all(repo: TagRepository = Depends()):
-    return repo.get_all()
+# ## GET ##
+# # getting detail feedback from employer
+# @router.get(
+#     "/get-tag/{Tag_id}",
+#     tags=["TagForm"],
+#     response_model=Union[TagOut, Error],
+# )
+# def get_one_employer_feedback_form(
+#     Tag_id: int,
+#     response: Response,
+#     repo: TagRepository = Depends(),
+# ) -> TagOut:
+#     Tag = repo.get_one(Tag_id)
+#     if Tag is None:
+#         response.status_code = 404
+#     return Tag
 
 
-## DELETE ##
-# Delete tag #
-@router.delete("/delete_tag/{Tag_id}", tags=["TagForm"], response_model=bool)
-def delete_tag(Tag_id: int, repo: TagRepository = Depends()):
-    return repo.delete(Tag_id)
+# ## GET ##
+# # getting list of tags
+# @router.get(
+#     "/get_all_tags", tags=["TagForm"], response_model=Union[List[TagOut], Error]
+# )
+# def get_all(repo: TagRepository = Depends()):
+#     return repo.get_all()
+
+
+# ## DELETE ##
+# # Delete tag #
+# @router.delete("/delete_tag/{Tag_id}", tags=["TagForm"], response_model=bool)
+# def delete_tag(Tag_id: int, repo: TagRepository = Depends()):
+#     return repo.delete(Tag_id)
