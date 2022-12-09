551fb8c8c4017c4aaba65448f0d75fdd8e916822
Hideaki Hama <hideaki.hama@yahoo.com>
2022-12-09 13:57:03 -0800

    pushing to main

--- accountsapi/tests/test_feedback.py
+++ accountsapi/tests/test_feedback.py
@@ -1,13 +1,35 @@
 # test_feedback.py
+from fastapi.testclient import TestClient
+from main import app
+from authenticator import MyAuthenticator
+from queries.EmployeeFeedback_queries import EmployeeFeedbackFormOut2
+from routers.EmployeeFeedback import EmployeeFeedbackRepository
+from datetime import date
 
-# Arrange
+client = TestClient(app)
 
-# input =
 
-# Act
+def fake_get_current_user():
+    return {}
 
-# result =
 
-# Assert
+feedback_out = EmployeeFeedbackFormOut2(
+    id=0,
+    employer_name="Aki",
+    date="2022-12-12",
+    description="Hello",
+    account_id=1,
+)
 
-# assert result =
+
+class fakeFeedbackRepository:
+    def get_all_feedbacks(self):
+        return feedback_out  # Change it
+
+
+def test_read_feedback():
+    app.dependency_overrides[MyAuthenticator] = fake_get_current_user
+    app.dependency_overrides[EmployeeFeedbackRepository] = fakeFeedbackRepository
+    response = client.get("/get_all_employeeFeedbacks")
+    assert response.status_code == 200
+    assert response.json() == feedback_out.dict()
